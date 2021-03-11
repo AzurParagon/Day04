76e01cd491718d681f87e9e6cedcd715760717c3
Azur Ramic <azurramic@gmail.com>
2021-03-11 20:03:09 +0100

    Add finished task

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function main(numOne, numTwo){
+    // Only change code below this line
+   var myQuotient = numOne / numTwo;
+    // Only change code above this line
+    return myQuotient;
+
+}
+
+console.log(main(102.0, 25.5));    // Change this line
+module.exports = main;
