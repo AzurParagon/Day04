33d869d277e1664a9bda0a0e1601184e59b98f91
Azur Ramic <azurramic@gmail.com>
2021-03-11 21:25:41 +0100

    Add finished task

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+// Only change code below this line
+var myFirstName = "Azur";
+var myLastName = "Ramic";
+var myHomeTown = "Tuzla";
+// Only change code above this line
+
+return{
+    myFirstName,
+    myLastName,
+    myHomeTown
+    }; 
+
+}
+
+console.log(main());
+module.exports = main;
+
